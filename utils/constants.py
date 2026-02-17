5d8383a7b76e921d052bed1e33d85ed496a2e725
leroidespingouin <147637947+azur-goat@users.noreply.github.com>
2026-02-17 21:26:03 +0100

    Add constants for application configuration

---
+++ utils/constants.py
@@ -0,0 +1,9 @@
+NUKE_THRESHOLD = 3
+NUKE_WINDOW = 10  # secondes
+
+MIN_ACCOUNT_AGE_DAYS = 7
+
+TOKEN_DETECTED_MSG = "🚨 Token Discord détecté et supprimé."
+UNAUTHORIZED_ACTION = "Unauthorized action detected"
+
+PARANO_MODE_DEFAULT = True
