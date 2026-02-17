065000618fe5392dde02912ad48a031ca14ecd87
leroidespingouin <147637947+azur-goat@users.noreply.github.com>
2026-02-17 21:05:20 +0100

    Add security features to ProtectBot

--- core/bot.py
+++ core/bot.py
@@ -5,6 +5,10 @@ from core.restore_manager import RestoreManager
 from core.freeze_manager import FreezeManager
 from core.crypto_manager import CryptoManager
 from core.anti_token_manager import AntiTokenManager
+from security.anti_alt import AntiAlt
+from security.anti_selfbot import AntiSelfbot
+from security.audit_analyzer import AuditAnalyzer
+from security.threshold_system import ThresholdSystem
 
 class ProtectBot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -16,3 +20,8 @@ class ProtectBot(commands.Bot):
         self.freeze_manager = FreezeManager(self)
         self.crypto_manager = CryptoManager()
         self.anti_token_manager = AntiTokenManager(self)
+        self.anti_alt = AntiAlt(self)
+        self.anti_selfbot = AntiSelfbot(self)
+        self.audit_analyzer = AuditAnalyzer(self)
+        self.threshold_system = ThresholdSystem()
+
